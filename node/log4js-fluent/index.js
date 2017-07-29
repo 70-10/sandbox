441313930c620a5fff579e8135d7243c7722c7b8
70_10 <7010.dev@gmail.com>
2017-07-29 22:27:09 +0900

    [add] elasticsearch, kibana

--- node/log4js-fluent/index.js
+++ node/log4js-fluent/index.js
@@ -1,6 +1,8 @@
 const log4js = require("log4js");
 const fluent = require("fluent-logger");
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 log4js.addAppender(
   fluent.support.log4jsAppender("log4js", {
     host: "localhost",
@@ -10,12 +12,19 @@ log4js.addAppender(
 );
 
 const log = getLogger("DEBUG");
-log.trace("trace");
-log.debug("debug");
-log.info("info");
-log.warn("warn");
-log.error("error");
-log.fatal("fatal");
+logging().catch(console.error)
+
+async function logging() {
+  while (true) {
+    log.trace("trace");
+    log.debug("debug");
+    log.info("info");
+    log.warn("warn");
+    log.error("error");
+    log.fatal("fatal");
+    await sleep(1000);
+  }
+}
 
 function getLogger(level) {
   log4js.addAppender(
