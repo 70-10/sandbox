f2a6c11aed0f752ef63bee9f54c140ee2fee6b3a
70_10 <7010.dev@gmail.com>
2017-08-09 15:09:31 +0900

    [add] scratch-deep-learning

---
+++ python/scratch-deep-learning/1/numpy-practice.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+x = np.array([1.0,2.0,3.0])
+y = np.array([2.0, 4.0, 6.0])
+
+print("x = %s" % x)
+print("type(x) = %s" % type(x))
+
+print("x + y = %s" % (x + y))
+print("x - y = %s" % (x - y))
+print("x * y = %s" % (x * y))
+print("x / y = %s" % (x / y))
+
+print("x / 2 = %s" % (x / 2))
