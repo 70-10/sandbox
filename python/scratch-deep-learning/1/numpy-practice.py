1498a6ace6e69a6b61600e2fe0e3a0f957a5721f
70_10 <7010.dev@gmail.com>
2017-08-10 14:13:38 +0900

    [mod] scratch-deep-lerning

--- python/scratch-deep-learning/1/numpy-practice.py
+++ python/scratch-deep-learning/1/numpy-practice.py
@@ -12,3 +12,22 @@ print("x * y = %s" % (x * y))
 print("x / y = %s" % (x / y))
 
 print("x / 2 = %s" % (x / 2))
+
+
+A = np.array([[1,2], [3,4]])
+print("A = %s" % A)
+print("A shape = %s" % (A.shape,))
+print("A dtype = %s" % A.dtype)
+
+B = np.array([[3, 0], [0, 6]])
+
+print("A + B = %s" % (A + B))
+print("A * B = %s" % (A * B))
+
+
+# Broadcast
+A = np.array([[1,2], [3,4]])
+B = np.array([10, 20])
+
+print("A * 10 = %s" % (A * 10))
+print("A * B = %s" % (A * B))
