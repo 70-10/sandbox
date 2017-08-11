5f8b6910383d6c92c76f9db590a1d5dc41cf3832
70_10 <7010.dev@gmail.com>
2017-08-11 23:47:27 +0900

    [mod] sdl 1.5.6

--- python/scratch-deep-learning/1/numpy-practice.py
+++ python/scratch-deep-learning/1/numpy-practice.py
@@ -25,9 +25,25 @@ print("A + B = %s" % (A + B))
 print("A * B = %s" % (A * B))
 
 
-# Broadcast
+# 1.5.5 Broadcast
 A = np.array([[1,2], [3,4]])
 B = np.array([10, 20])
 
 print("A * 10 = %s" % (A * 10))
 print("A * B = %s" % (A * B))
+
+# 1.5.6
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print("X = %s" % X)
+print("X[0] = %s" % X[0])
+print("X[0][1] = %s" % X[0][1])
+
+print("Loop X = ")
+for row in X:
+    print(row)
+
+Y = X.flatten()
+print("Y = %s" % Y)
+print("Y[np.array([0,2,4])] = %s" % Y[np.array([0,2,4])])
+print("Y>15 = %s" % (Y > 15))
+print("Y[Y>15] = %s" % (Y[Y>15]))
