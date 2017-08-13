1e61a0ab42406664b4764e6f640686296bfad09a
70_10 <7010.dev@gmail.com>
2017-08-13 22:13:28 +0900

    [add] sdl 2

---
+++ python/scratch-deep-learning/2/perceptron.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print("AND(0, 0)  = %d" % AND(0, 0))
+print("AND(1, 0)  = %d" % AND(1, 0))
+print("AND(0, 1)  = %d" % AND(0, 1))
+print("AND(1, 1)  = %d" % AND(1, 1))
+
+print("NAND(0, 0) = %d" % NAND(0, 0))
+print("NAND(1, 0) = %d" % NAND(1, 0))
+print("NAND(0, 1) = %d" % NAND(0, 1))
+print("NAND(1, 1) = %d" % NAND(1, 1))
+
+print("OR(0, 0)   = %d" % OR(0, 0))
+print("OR(1, 0)   = %d" % OR(1, 0))
+print("OR(0, 1)   = %d" % OR(0, 1))
+print("OR(1, 1)   = %d" % OR(1, 1))
