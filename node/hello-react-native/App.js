dbb9fb6fdb914012559b504fc93106f875d92b35
70_10 <7010.dev@gmail.com>
2017-08-21 00:30:39 +0900

    [add] native-base from hello-react-native

--- node/hello-react-native/App.js
+++ node/hello-react-native/App.js
@@ -1,63 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+import {
+  Container,
+  Header,
+  Left,
+  Right,
+  Body,
+  Title,
+  Button,
+  Icon,
+  Content
+} from "native-base";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Greeting name="React Native1" />
-        <Greeting name="React Native2" />
-        <Greeting name="React Native3" />
-        <Blink text="Blink Text" />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  big: {
-    fontWeight: "bold",
-    fontSize: 30
-  },
-  blue: {
-    color: "blue"
-  }
-});
-
-class Greeting extends React.Component {
-  render() {
-    return (
-      <Text>
-        Hello {this.props.name}!
-      </Text>
-    );
-  }
-}
-
-class Blink extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { showText: true };
-
-    setInterval(() => {
-      this.setState(previousState => {
-        return { showText: !previousState.showText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.showText ? this.props.text : "";
-    return (
-      <Text style={[styles.big, styles.blue]}>
-        {display}
-      </Text>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Hello React Native</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Right>
+        </Header>
+        <Content>
+          <Text>Hello React Native!!</Text>
+        </Content>
+      </Container>
     );
   }
 }
