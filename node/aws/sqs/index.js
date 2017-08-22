47cef2335f9028777b13c6a1ab60ff92b3496c93
70_10 <7010.dev@gmail.com>
2017-08-22 10:48:40 +0900

    [mod] node/aws/sqs: send and receive message

--- node/aws/sqs/index.js
+++ node/aws/sqs/index.js
@@ -4,16 +4,67 @@ AWS.config.update({
 });
 AWS.config.setPromisesDependency(Promise);
 
-async function run() {
-  const SQS = new AWS.SQS({
-    endpoint: "http://localhost:4576",
-    region: "ap-northeast-1"
-  });
+const QUEUE_NAME = "test-queue";
 
-  const result = await SQS.getQueueUrl({
-    QueueName: "test-queue"
-  }).promise()
-  console.log(result);
+const SQS = new AWS.SQS({
+  endpoint: "http://localhost:4576",
+  region: "ap-northeast-1"
+});
+
+async function main() {
+  const { QueueUrl } = await createQueue(SQS, QUEUE_NAME);
+  sendMessages(SQS, QueueUrl).catch(console.error);
+  receiveAndDelete(SQS, QueueUrl, 3).catch(console.error);
 }
 
-run().catch(console.error)
+main().catch(console.error);
+
+async function receiveAndDelete(sqs, queueUrl, receiveCount) {
+  const { Messages } = await sqs
+    .receiveMessage({
+      QueueUrl: queueUrl,
+      MaxNumberOfMessages: receiveCount
+    })
+    .promise();
+  if (Messages) {
+    Messages.forEach(async msg => {
+      console.log(msg.Body);
+      await sqs
+        .deleteMessage({
+          QueueUrl: queueUrl,
+          ReceiptHandle: msg.ReceiptHandle
+        })
+        .promise();
+    });
+  } else {
+    console.log("Message is nothing.");
+  }
+  await sleep(2000);
+
+  setImmediate(async () => {
+    await receiveAndDelete(sqs, queueUrl, receiveCount);
+  });
+}
+
+async function sendMessages(sqs, queueUrl) {
+  let i = 0;
+  while (true) {
+    console.log(`Send Message: ${i}`);
+    await sqs
+      .sendMessage({
+        QueueUrl: queueUrl,
+        MessageBody: `Message: ${i}`
+      })
+      .promise();
+    await sleep(500);
+    i++;
+  }
+}
+
+async function createQueue(sqs, queueName) {
+  return await sqs.createQueue({ QueueName: queueName }).promise();
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
