847108fe3992577789cf8ae88ab2632002500c78
70_10 <7010.dev@gmail.com>
2017-09-12 12:51:34 +0900

    [add] node/serverless/ecma-pra: scripts

--- node/serverless/ecma-pra/webpack.config.js
+++ node/serverless/ecma-pra/webpack.config.js
@@ -1,21 +1,23 @@
-const path = require('path');
+const path = require("path");
 // eslint-disable-next-line import/no-unresolved
-const slsw = require('serverless-webpack');
+const slsw = require("serverless-webpack");
 
 module.exports = {
   entry: slsw.lib.entries,
-  target: 'node',
+  target: "node",
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        include: __dirname,
+        exclude: /node_modules/
+      }
+    ]
   },
   output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js"
+  }
 };
