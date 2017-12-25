cc120c458f5b1d368f771d0d8e433d080d5fd7bf
70_10 <7010.dev@gmail.com>
2017-12-25 14:51:08 +0900

    [add] node/socketio-chat: socket.io-client

--- node/socketio-chat/index.js
+++ node/socketio-chat/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 const mecab = require("./lib/mecab");
+const client = require("socket.io-client");
 
 const app = express();
 const server = http.Server(app);
@@ -11,15 +12,28 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+app.post("/call", (req, res) => {
+  const socket = client.connect("http://localhost:3000/");
+  socket.on("connect", () => {
+    console.log("caller connect");
+    socket.emit("chat message", { type: "caller", message: "caller message" });
+    socket.disconnect();
+  });
+  res.status(200).end();
+});
+
 io.on("connection", socket => {
   console.log(`a user connected`);
   socket.broadcast.emit("hi");
 
   socket.on("disconnect", () => console.log("user disconnected"));
   socket.on("chat message", async msg => {
-    io.emit("chat message", msg);
-    const text = await mecab.pParse(msg);
-    io.emit("chat message", text);
+    // msg = {type: "string", message: "string"}
+    io.emit("chat message", msg.message);
+    if (msg.type === "browser") {
+      const text = await mecab.pParse(msg.message);
+      io.emit("chat message", text);
+    }
   });
 });
 
