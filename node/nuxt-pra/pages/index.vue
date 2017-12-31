f663315fd58b2280504f73662e34830b0daf9f03
70_10 <7010.dev@gmail.com>
2017-12-31 18:27:47 +0900

    [add] node/nuxt-pra

---
+++ node/nuxt-pra/pages/index.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div
+  .hero.is-primary
+    .hero-body
+      .container
+        h1.title Next Practice
+  section.section
+    .container
+      ul
+        li(v-for="item in list") {{ item.date }} {{ item.album.name }} - {{ item.artist.name }}
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    let data = await app.$axios.$get("/listened");
+    return { list: data };
+  }
+};
+</script>
+
+<style lang="sass">
+</style>
