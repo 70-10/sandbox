27ed617b4ffec987eb97be1809abf4653f29f15d
70_10 <7010.dev@gmail.com>
2018-01-22 01:10:41 +0900

    [add] node/express-ghn: ngrok

--- node/express-ghn/server.js
+++ node/express-ghn/server.js
@@ -1,10 +1,11 @@
+const ngrok = require("ngrok");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const googlehome = require("google-home-notifier");
 
 const port = process.env.PORT || 3000;
-
+let PublicURL = "";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -13,6 +14,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   next();
 });
+
+app.use(express.static("public"));
 
 app.post("/", async (req, res) => {
   const language = req.body.language;
@@ -24,14 +27,33 @@ app.post("/", async (req, res) => {
   res.send({ status: "OK" });
 });
 
+app.get("/play", async (req, res) => {
+  const url = `${PublicURL}/omake1.mp3`;
+  console.log(url);
+  const result = await play(url);
+  res.send({ status: "OK", result });
+});
+
 async function main() {
-  app.listen(port);
-  console.log(`Server start: ${port}`);
+  app.listen(port, async () => {
+    PublicURL = await ngrokConnect(port);
+    console.log(`Server start: ${port}`);
+    console.log(`ngrok: ${PublicURL}`);
+  });
 }
 
 function notify(message, language) {
   googlehome.device("Google-Home", language || "ja");
   return new Promise(resolve => googlehome.notify(message, resolve));
+}
+
+function play(mp3Url) {
+  googlehome.device("Google-Home", "ja");
+  return new Promise(resolve => googlehome.play(mp3Url, resolve));
+}
+
+function ngrokConnect(port) {
+  return new Promise((resolve, reject) => ngrok.connect(port, (err, url) => (err ? reject(err) : resolve(url))));
 }
 
 main().catch(console.error);
