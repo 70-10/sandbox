90673302146562b0d44dbbb018e5048a424c8848
70_10 <7010.dev@gmail.com>
2018-02-08 16:54:25 +0900

    [add] node/danmaku-chat: comment and viewer count, clap button

--- node/danmaku-chat/server.js
+++ node/danmaku-chat/server.js
@@ -7,18 +7,31 @@ const server = http.Server(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3000;
+let viewerCount = 0;
+let commentCount = 0;
 
 app.use("/static", express.static("public"));
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 io.on("connection", socket => {
   console.log(`a user connected`);
-  socket.broadcast.emit("hi");
 
-  socket.on("disconnect", () => console.log("user disconnected"));
+  viewerCount++;
+  console.log(viewerCount);
+  io.emit("count", viewerCount);
+  io.emit("comment-count", commentCount);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    viewerCount--;
+    console.log(viewerCount);
+    io.emit("count", viewerCount);
+  });
   socket.on("chat message", async msg => {
+    console.log(msg);
     // msg = {type: "string", message: "string"}
     io.emit("chat message", msg.message);
+    io.emit("comment-count", ++commentCount);
   });
 });
 
