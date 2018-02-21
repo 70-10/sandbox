2b74a38fffe46cf9c10749e22048ebe97db58279
70_10 <7010.dev@gmail.com>
2018-02-21 18:25:15 +0900

    [mod] node/reverse-proxy-express

--- node/reverse-proxy-express/app.js
+++ node/reverse-proxy-express/app.js
@@ -3,9 +3,14 @@ const app = express();
 const httpProxy = require("http-proxy");
 const apiProxy = httpProxy.createProxyServer();
 
-app.all("/app1*", (req, res) => {
+app.get("/hc", (req, res) => {
+  res.send({ status: "OK", port: 3000 });
+});
+app.all("/*", (req, res) => {
   console.log("redirecting to port 3001");
   apiProxy.web(req, res, { target: "http://localhost:3001" });
 });
 
-module.exports = app.listen(3000);
+module.exports = app.listen(3000, () => {
+  console.log("port 3000");
+});
--- node/reverse-proxy-express/server.js
+++ node/reverse-proxy-express/server.js
@@ -5,4 +5,6 @@ app.get("/app1", (req, res) => {
   res.send("Hello world");
 });
 
-module.exports = app.listen(3001);
+module.exports = app.listen(3001, () => {
+  console.log("port 3001");
+});
