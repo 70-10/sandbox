9a32bf21cd8606697b704dd3c8ebe647ebfd4db5
70_10 <7010.dev@gmail.com>
2018-06-20 23:31:00 +0900

    [mod] node/swagger-parser: yml -> js

--- node/swagger-yaml/index.js
+++ node/swagger-yaml/index.js
@@ -1,25 +1,16 @@
-const { resolveRefs } = require("json-refs");
 const yaml = require("js-yaml");
 const path = require("path");
-const fs = require("fs");
 
-async function parse(filePath) {
-  const root = yaml.load(fs.readFileSync(filePath).toString());
-  const options = {
-    filter: ["relative", "remote"],
-    loaderOptions: {
-      processContent: (res, callback) => callback(null, yaml.safeLoad(res.text))
-    }
-  };
-  const result = await resolveRefs(root, options);
-  return yaml.dump(result.resolved);
+function pathResolve(filePath) {
+  if (path.isAbsolute(filePath)) {
+    return filePath;
+  }
+
+  return path.join(process.cwd(), filePath);
 }
 
 module.exports = swaggerFile => {
-  const filePath = swaggerFile
-    ? path.join(process.cwd(), swaggerFile)
-    : path.join(process.cwd(), "index.yml");
-  parse(filePath)
-    .then(console.log)
-    .catch(console.error);
+  const filePath = pathResolve(swaggerFile || "index.js");
+
+  process.stdout.write(yaml.dump(require(filePath)));
 };
