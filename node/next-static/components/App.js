2dcbfa447b6a041e7fb507d4e7797a3f312d293d
70_10 <7010.dev@gmail.com>
2018-07-06 14:03:04 +0900

    [mod] node/next-static: change primary color

--- node/next-static/components/App.js
+++ node/next-static/components/App.js
@@ -2,7 +2,7 @@ import "../styles/app.scss";
 import Header from "../containers/Header";
 
 const App = ({ children }) => (
-  <section className="hero is-fullheight is-light">
+  <section className="hero is-fullheight is-primary">
     <div className="hero-head">
       <Header />
     </div>
