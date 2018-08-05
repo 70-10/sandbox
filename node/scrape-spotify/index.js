2717a93b7f65ad4d700c73980232e3e6c7d15434
70_10 <7010.dev@gmail.com>
2018-08-05 23:11:28 +0900

    [mod] node/scrape-spotify: arg

--- node/scrape-spotify/index.js
+++ node/scrape-spotify/index.js
@@ -1,9 +1,12 @@
 const puppeteer = require("puppeteer");
 
 async function main() {
-  const url =
-    "https://open.spotify.com/album/5ZQpSAWUWZomHrfFbKvfrc?si=V_ffnis0QGyKTnCo2W73YQ";
+  if (process.argv.length <= 2) {
+    console.error("Please album id");
+    return;
+  }
 
+  const url = `https://open.spotify.com/album/${getID(process.argv[2])}`;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -12,9 +15,17 @@ async function main() {
   const title = await page.$eval(".primary-title", i => i.textContent);
   const artist = await page.$eval(".secondary-title", i => i.textContent);
 
-  console.log(`${title} - ${artist}`);
+  console.log(`artist: ${artist}`);
+  console.log(`title : ${title}`);
 
   browser.close();
+}
+
+function getID(str) {
+  if (str.match(/^spotify:/)) {
+    return str.split("spotify:")[1];
+  }
+  return str;
 }
 
 main().catch(console.error);
