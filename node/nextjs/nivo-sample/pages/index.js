17b7380bf210fa56eb2112cae1a2c20948162e52
70_10 <7010.dev@gmail.com>
2018-08-21 23:31:30 +0900

    [add] nextjs/nivo-sample: pie chart

--- node/nextjs/nivo-sample/pages/index.js
+++ node/nextjs/nivo-sample/pages/index.js
@@ -1,5 +1,6 @@
 import "../styles/app.scss";
 import Calendar from "../components/Calendar";
+import Pie from "../components/PieChart";
 
 export default () => (
   <section className="section">
@@ -17,6 +18,46 @@ export default () => (
                 {
                   day: "2018-08-11",
                   value: 800
+                }
+              ]}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column is-7">
+          <div style={{ width: "100%", height: "400px" }}>
+            <Pie
+              data={[
+                {
+                  id: "javascript",
+                  label: "javascript",
+                  value: 265,
+                  color: "hsl(168, 70%, 50%)"
+                },
+                {
+                  id: "ruby",
+                  label: "ruby",
+                  value: 341,
+                  color: "hsl(113, 70%, 50%)"
+                },
+                {
+                  id: "php",
+                  label: "php",
+                  value: 432,
+                  color: "hsl(70, 70%, 50%)"
+                },
+                {
+                  id: "java",
+                  label: "java",
+                  value: 445,
+                  color: "hsl(205, 70%, 50%)"
+                },
+                {
+                  id: "elixir",
+                  label: "elixir",
+                  value: 420,
+                  color: "hsl(111, 70%, 50%)"
                 }
               ]}
             />
