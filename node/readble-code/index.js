262db1b96f12cabe286d95571a431c15a6c009e7
70_10 <7010.dev@gmail.com>
2018-09-01 22:49:08 +0900

    [mod] node/readble-code: separate1, 2

--- node/readble-code/index.js
+++ node/readble-code/index.js
@@ -1,5 +1,5 @@
 async function main() {
-  const state = [
+  const users = [
     { name: "James", gender: "male" },
     { name: "Mary", gender: "female" },
     { name: "John", gender: "male" },
@@ -8,19 +8,35 @@ async function main() {
     { name: "Robert", gender: "male" },
   ];
 
+  const result1 = separate1(users);
+
+  console.log(`males  : ${JSON.stringify(result1.males)}`);
+  console.log(`females: ${JSON.stringify(result1.females)}`);
+
+  const result2 = separate2(users);
+  console.log(`males  : ${JSON.stringify(result2.males)}`);
+  console.log(`females: ${JSON.stringify(result2.females)}`);
+}
+
+function separate1(users) {
   let males = [];
   let females = [];
 
-  state.forEach(i => {
+  users.forEach(i => {
     if (i.gender === "male") {
       males.push(i);
     } else {
       females.push(i);
     }
   });
+  return { males, females };
+}
 
-  console.log(`males: ${JSON.stringify(males)}`);
-  console.log(`females: ${JSON.stringify(females)}`);
+function separate2(users) {
+  return {
+    males: users.filter(user => user.gender === "male"),
+    females: users.filter(user => user.gender === "female"),
+  };
 }
 
 main().catch(console.error);
