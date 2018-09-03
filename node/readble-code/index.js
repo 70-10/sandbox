830bf498d2b14ac0411d792422fc51ea10f81c59
70_10 <7010.dev@gmail.com>
2018-09-03 19:02:42 +0900

    [mod] node/readble-code: benchmark

--- node/readble-code/index.js
+++ node/readble-code/index.js
@@ -1,21 +1,36 @@
+const times = require("lodash.times");
+
+const Gender = {
+  Male: 0,
+  Female: 1,
+};
 async function main() {
-  const users = [
-    { name: "James", gender: "male" },
-    { name: "Mary", gender: "female" },
-    { name: "John", gender: "male" },
-    { name: "Patricia", gender: "female" },
-    { name: "Linda", gender: "female" },
-    { name: "Robert", gender: "male" },
-  ];
+  const execCounts = [10, 100, 1000, 10000, 100000, 1000000, 10000000];
 
-  const result1 = separate1(users);
+  execCounts.forEach(count => {
+    console.log(`==================== count = ${count} ====================`);
+    times(10).forEach(i => {
+      console.log(`count: ${i}`);
+      const users = createDummyUsers(count);
 
-  console.log(`males  : ${JSON.stringify(result1.males)}`);
-  console.log(`females: ${JSON.stringify(result1.females)}`);
+      console.time("separate1");
+      const result1 = separate1(users);
+      console.timeEnd("separate1");
 
-  const result2 = separate2(users);
-  console.log(`males  : ${JSON.stringify(result2.males)}`);
-  console.log(`females: ${JSON.stringify(result2.females)}`);
+      // console.log(`males  : ${JSON.stringify(result1.males)}`);
+      // console.log(`females: ${JSON.stringify(result1.females)}`);
+
+      console.time("separate2");
+      const result2 = separate2(users);
+      console.timeEnd("separate2");
+      // console.log(`males  : ${JSON.stringify(result2.males)}`);
+      // console.log(`females: ${JSON.stringify(result2.females)}`);
+    });
+  });
+}
+
+function createDummyUsers(num) {
+  return times(num, i => ({ name: `${i}`, gender: Math.floor(Math.random() * 2) }));
 }
 
 function separate1(users) {
@@ -23,7 +38,7 @@ function separate1(users) {
   let females = [];
 
   users.forEach(i => {
-    if (i.gender === "male") {
+    if (i.gender === Gender.Male) {
       males.push(i);
     } else {
       females.push(i);
@@ -34,8 +49,8 @@ function separate1(users) {
 
 function separate2(users) {
   return {
-    males: users.filter(user => user.gender === "male"),
-    females: users.filter(user => user.gender === "female"),
+    males: users.filter(user => user.gender === Gender.Male),
+    females: users.filter(user => user.gender === Gender.Female),
   };
 }
 
