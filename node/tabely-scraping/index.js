4dbb4f699578c2b7ae73f81c37bc0a35c6f7e988
70_10 <7010.dev@gmail.com>
2018-09-12 22:59:16 +0900

    [mod] node/tabely-scraping: get menu name

--- node/tabely-scraping/index.js
+++ node/tabely-scraping/index.js
@@ -4,12 +4,16 @@ async function main() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://tabe.ly/");
-  const list = await page.$$eval(".new-recipes .column a", list =>
-    list.map(d => ({ url: d.href }))
-  );
-  console.log(list);
-  // await page.screenshot({ path: "example.png" });
 
+  const result = await page.evaluate(
+    selector =>
+      Array.from(document.querySelectorAll(selector)).map(d => ({
+        name: d.querySelector(".menu-name").textContent,
+        url: d.href
+      })),
+    ".new-recipes .column a"
+  );
+  console.log(result);
   await browser.close();
 }
 
