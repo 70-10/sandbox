2a5cff44f5271414fbce61992de9c36fa8edf819
70_10 <7010.dev@gmail.com>
2018-10-01 23:39:53 +0900

    [mod] node/electron/webview-pra: ipc

--- node/electron/webview-pra/main.js
+++ node/electron/webview-pra/main.js
@@ -1,10 +1,14 @@
-const {app, BrowserWindow} = require("electron")
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({width:800, height:600})
-    win.loadFile("index.html")
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile("index.html");
 }
 
-app.on("ready", createWindow)
+app.on("ready", createWindow);
+
+ipcMain.on("link", (event, args) => {
+  event.sender.send("reply", "https://www.google.co.jp");
+});
