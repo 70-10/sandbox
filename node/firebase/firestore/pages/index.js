639b6c83138435008e23016cb6158ee8cf1a5cb4
70_10 <7010.dev@gmail.com>
2018-10-03 23:47:59 +0900

    [mod] node/firebase/firestore: get and post message

--- node/firebase/firestore/pages/index.js
+++ node/firebase/firestore/pages/index.js
@@ -14,7 +14,9 @@ const config = {
 export default class Top extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", mail: "" };
+    this.state = { text: "", messages: [] };
+    this.changeMessage = this.changeMessage.bind(this);
+    this.postMessage = this.postMessage.bind(this);
   }
 
   async componentDidMount() {
@@ -22,22 +24,55 @@ export default class Top extends Component {
     const db = firebase.firestore();
     const settings = { timestampsInSnapshots: true };
     db.settings(settings);
-    await db.collection("users").add({
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    });
 
-    const querySnapshot = await db.collection("users").get();
-    querySnapshot.forEach(doc => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
+    const querySnapshot = await db
+      .collection("romms")
+      .doc("roomA")
+      .collection("messages")
+      .get();
+
+    const messages = [];
+    querySnapshot.forEach(doc => messages.push(doc.data()));
+    this.setState({ messages });
+    console.log(this.state.messages);
+  }
+
+  changeMessage(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  async postMessage() {
+    const db = firebase.firestore();
+    const settings = { timestampsInSnapshots: true };
+    db.settings(settings);
+    await db
+      .collection("romms")
+      .doc("roomA")
+      .collection("messages")
+      .add({
+        message: this.state.text,
+        date: new Date()
+      });
+
+    this.setState({ text: "" });
   }
 
   render() {
     return (
       <div>
         <h1>Firestore Sample</h1>
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={this.changeMessage}
+        />
+        <button onClick={this.postMessage}>追加</button>
+
+        <ul>
+          {this.state.messages.map(m => (
+            <li>{m.message}</li>
+          ))}
+        </ul>
       </div>
     );
   }
