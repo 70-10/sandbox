c69d70f48372b22fc474236a14b6eb3d266d4bb9
70_10 <7010.dev@gmail.com>
2018-10-17 20:03:39 +0900

    [mod] node/tabely-scraping: headless true

--- node/tabely-scraping/index.js
+++ node/tabely-scraping/index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function main() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto("https://tabe.ly/");
 
