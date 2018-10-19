1570c21f36eb7e4c2fe16683bd5ab6906d5753bf
70_10 <7010.dev@gmail.com>
2018-10-19 23:17:46 +0900

    [mod] node/vue/nyumon: computed

--- node/vue/nyumon/ch02/main.js
+++ node/vue/nyumon/ch02/main.js
@@ -1,9 +1,9 @@
 console.assert(typeof Vue !== "undefined");
 
 const items = [
-  { name: "鉛筆", price: 300, quantity: 0 },
-  { name: "ノート", price: 400, quantity: 0 },
-  { name: "消しゴム", price: 500, quantity: 0 }
+  { name: "鉛筆", price: 300, quantity: 1 },
+  { name: "ノート", price: 400, quantity: 1 },
+  { name: "消しゴム", price: 500, quantity: 1 }
 ];
 
 const vm = new Vue({
@@ -17,6 +17,17 @@ const vm = new Vue({
         return "0";
       }
       return value.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+    }
+  },
+  computed: {
+    totalPrice() {
+      return this.items.reduce(
+        (sum, item) => sum + item.price * item.quantity,
+        0
+      );
+    },
+    totalPriceWithTax() {
+      return Math.floor(this.totalPrice * 1.08);
     }
   }
 });
