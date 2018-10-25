ed92b51e8067c4400569fd89257ec472b5a74024
70_10 <7010.dev@gmail.com>
2018-10-25 22:37:16 +0900

    [mod] vue/nyumon: ch03

--- node/vue/nyumon/ch03/main.js
+++ node/vue/nyumon/ch03/main.js
@@ -1,17 +1,69 @@
-Vue.component("fruits-item-name", {
-  props: {
-    fruitsItem: {
-      type: Object,
-      required: true
-    }
-  },
-  template: "<li>{{ fruitsItem.name }}</li>"
+const auth = {
+  login(id, pass) {
+    window.alert(`userid: ${id}\npassword: ${pass}`);
+  }
+};
+const headerTemplate = `
+  <div>
+    <slot name="header">No title</slot>
+  </div>
+`;
+
+const contentTemplate = `
+  <div>
+    <slot name="content">No contents</slot>
+  </div>
+`;
+
+Vue.component("page-header", {
+  template: headerTemplate
 });
 
-const vm = new Vue({
-  el: "#fruits-component",
-  data: {
-    fruitsItems: [{ name: "梨" }, { name: "イチゴ" }]
+Vue.component("page-content", {
+  template: contentTemplate
+});
+
+Vue.component("user-login", {
+  template: "#login-template",
+  data() {
+    return {
+      userid: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      auth.login(this.userid, this.password);
+    }
   }
+});
+// const counterButton = Vue.extend({
+//   template:
+//     '<span>{{ counter }}個<button v-on:click="addToCart">追加</button></span>',
+//   data() {
+//     return { counter: 0 };
+//   },
+//   methods: {
+//     addToCart() {
+//       this.counter++;
+//       this.$emit("increment");
+//     }
+//   }
+// });
+
+const vm = new Vue({
+  el: "#login-example"
+  // components: {
+  //   "counter-button": counterButton
+  // },
+  // data: {
+  //   total: 0,
+  //   fruits: [{ name: "梨" }, { name: "イチゴ" }]
+  // },
+  // methods: {
+  //   incrementCartStatus() {
+  //     this.total++;
+  //   }
+  // }
 });
 window.vm = vm;
