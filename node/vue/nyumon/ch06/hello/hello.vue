a4f9f68cad354fe1992e4b1c78707c9b3ee320e8
70_10 <7010.dev@gmail.com>
2018-11-01 22:42:19 +0900

    [add] vue/nyumon: ch06

---
+++ node/vue/nyumon/ch06/hello/hello.vue
@@ -0,0 +1,17 @@
+<template>
+    <p class="message">message: {{ msg }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {msg: "Hello, World"}
+    }
+}
+</script>
+
+<style>
+.message {
+  color: #42b983;
+}
+</style>
