2c8582c7a95c0553efc8bfb8a188bcc5cea391a7
70_10 <7010.dev@gmail.com>
2019-01-17 17:33:08 +0900

    [mod] gatsby/hello-world: done tutorial part 6

--- node/gatsby/hello-world/src/pages/index.js
+++ node/gatsby/hello-world/src/pages/index.js
@@ -1,9 +1,57 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { css } from "@emotion/core";
+import { rhythm } from "../utils/typography";
 import Layout from "../components/layout";
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <h1>Top Page</h1>
-    <p>This is top page</p>
+    <h1
+      css={css`
+        display: inline-block;
+        border-bottom: 1px solid;
+      `}
+    >
+      Top Page
+    </h1>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(1 / 4)};
+          `}
+        >
+          {node.frontmatter.title}{" "}
+          <span
+            css={css`
+              color: #bbb;
+            `}
+          >
+            - {node.frontmatter.date}
+          </span>
+        </h3>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "YYYY-MM-DD")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
---
+++ node/gatsby/hello-world/src/pages/sample-post.md
@@ -0,0 +1,8 @@
+---
+title: "Sample Post"
+date: "2019-01-17"
+---
+
+# Sample Post
+
+This is sample post
