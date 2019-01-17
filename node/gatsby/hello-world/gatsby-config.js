2aed666ea90c1e2bfd6f29c9b11130adef1f1063
70_10 <7010.dev@gmail.com>
2019-01-17 15:49:47 +0900

    [mod] gatsby/hello-world: done tutorial part 5

--- node/gatsby/hello-world/gatsby-config.js
+++ node/gatsby/hello-world/gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
     title: "Gatsby Practice"
   },
   plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src`
+      }
+    },
     "gatsby-plugin-emotion",
     {
       resolve: "gatsby-plugin-typography",
