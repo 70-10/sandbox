534189a87ab3ba74ab541859f23c46a3b396675d
70_10 <7010.dev@gmail.com>
2019-02-05 14:33:52 +0900

    [add] gatsby/sass-practice

---
+++ node/gatsby/sass-practice/src/components/layout.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./layout.scss";
+
+export default ({ children }) => (
+  <section className="section">{children}</section>
+);
---
+++ node/gatsby/sass-practice/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1 className="title">Hello World</h1>
+    <button className="button is-primary">Button</button>
+  </Layout>
+);
