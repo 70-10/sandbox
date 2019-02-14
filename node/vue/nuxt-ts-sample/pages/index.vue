6f62cfa645f88d48ddd406f58a5c0e0dc3b84c6a
70_10 <7010.dev@gmail.com>
2019-02-14 15:08:52 +0900

    [add] vue/nuxt-ts-sample

---
+++ node/vue/nuxt-ts-sample/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {
+  message: string = "Hello world !";
+}
+</script>
