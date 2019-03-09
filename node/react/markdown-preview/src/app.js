667972c0000d26255e661aa6ac4afa0ed76e2638
70_10 <7010.dev@gmail.com>
2019-03-09 15:12:37 +0900

    [mod] react/markdown-preview: react hooks

--- node/react/markdown-preview/src/app.js
+++ node/react/markdown-preview/src/app.js
@@ -1,41 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import remark from "remark";
 import remark2react from "remark-react";
 import "./app.scss";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { text: "# hello world" };
-    this.onChange = this.onChange.bind(this);
-  }
-  onChange(e) {
-    this.setState({ text: e.target.value });
-  }
-  render() {
-    return (
-      <section className="section">
-        <div className="columns">
-          <div className="column">
-            <textarea
-              className="textarea"
-              rows="30"
-              value={this.state.text}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className="column">
-            <div className="content">
-              {
-                remark()
-                  .use(remark2react)
-                  .processSync(this.state.text).contents
-              }
-            </div>
+export default () => {
+  const [text, setText] = useState("# hello");
+
+  return (
+    <section className="section">
+      <div className="columns">
+        <div className="column">
+          <textarea
+            className="textarea"
+            rows="30"
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+        </div>
+        <div className="column">
+          <div className="content">
+            {
+              remark()
+                .use(remark2react)
+                .processSync(text).contents
+            }
           </div>
         </div>
-      </section>
-    );
-  }
-}
-export default App;
+      </div>
+    </section>
+  );
+};
