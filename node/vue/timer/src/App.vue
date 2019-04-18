4f56fdadae5fa844820e63d2b32e845054f0a839
70_10 <7010.dev@gmail.com>
2019-04-18 23:19:31 +0900

    [mod] vue/timer

--- node/vue/timer/src/App.vue
+++ node/vue/timer/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="app">
-    {{ count }}
-    <div>
-      <button @click="add(1 * 60 * 60)">+ 1 hour</button>
-      <button @click="add(1 * 60)">+ 1 minute</button>
-      <button @click="add(1)">+ 1 second</button>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 id="title" class="title has-text-centered">{{ count }}</h1>
+        <progress class="progress is-success" :value="timer" :max="max">{{count}}</progress>
+        <div>
+          <button @click="add(1 * 60 * 60)">+ 1 hour</button>
+          <button @click="add(1 * 60)">+ 1 minute</button>
+          <button @click="add(1)">+ 1 second</button>
+        </div>
+        <button @click="start">start</button>
+        <button @click="stop">stop</button>
+        <button @click="reset">reset</button>
+      </div>
     </div>
-    <button @click="start">start</button>
-    <button @click="stop">stop</button>
-    <button @click="reset">reset</button>
-  </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -23,7 +28,8 @@ import numeral from "numeral";
   }
 })
 export default class App extends Vue {
-  timer: number = 100;
+  max: number = 0;
+  timer: number = 0;
   interval: number = 0;
 
   get count() {
@@ -49,13 +55,22 @@ export default class App extends Vue {
 
   add(num: number): void {
     this.timer += num;
+    this.max += num;
   }
 
   reset(): void {
     clearInterval(this.interval);
     this.timer = 0;
+    this.max = 0;
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#title {
+  font-size: 10rem;
+}
+progress {
+  height: 3rem;
+}
+</style>
