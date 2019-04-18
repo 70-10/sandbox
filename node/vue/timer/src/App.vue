d6696956ed47f8ba495a556f019c0ab539cd5722
70_10 <7010.dev@gmail.com>
2019-04-18 14:16:06 +0900

    [mod] vue/timer

--- node/vue/timer/src/App.vue
+++ node/vue/timer/src/App.vue
@@ -1,51 +1,61 @@
 <template>
   <div id="app">
-    <p>{{ formatCount }}</p>
-    <p>Timer</p>
-    <input type="text" :value="settings.timer" @input="updateTimer">
-    <button @click="setCount">設定</button>
-    <p>Interval</p>
-    <input type="text" :value="settings.interval">
+    {{ count }}
+    <div>
+      <button @click="add(1 * 60 * 60)">+ 1 hour</button>
+      <button @click="add(1 * 60)">+ 1 minute</button>
+      <button @click="add(1)">+ 1 second</button>
+    </div>
+    <button @click="start">start</button>
+    <button @click="stop">stop</button>
+    <button @click="reset">reset</button>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
 import numeral from "numeral";
 
-export default {
-  name: "app",
-  computed: {
-    count() {
-      return this.$store.getters.count;
-    },
-    formatCount() {
-      return numeral(this.$store.getters.count).format("00:00:00");
-    },
-    settings() {
-      return this.$store.getters.settings;
-    }
-  },
-  created() {
-    this.$store.dispatch("init");
-  },
-  methods: {
-    updateTimer(e) {
-      this.$store.commit("updateTimer", e.target.value);
-    },
-    setCount() {
-      this.$store.dispatch("init");
-    }
+@Component({
+  components: {
+    HelloWorld
   }
-};
+})
+export default class App extends Vue {
+  timer: number = 100;
+  interval: number = 0;
+
+  get count() {
+    return numeral(this.timer).format("00:00:00");
+  }
+
+  start(): void {
+    if (this.timer <= 0) {
+      return;
+    }
+    this.interval = setInterval(() => {
+      this.timer--;
+      if (this.timer <= 0) {
+        clearInterval(this.interval);
+        return;
+      }
+    }, 1000);
+  }
+
+  stop(): void {
+    clearInterval(this.interval);
+  }
+
+  add(num: number): void {
+    this.timer += num;
+  }
+
+  reset(): void {
+    clearInterval(this.interval);
+    this.timer = 0;
+  }
+}
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
