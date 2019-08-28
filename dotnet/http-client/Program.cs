8de5185c32a82c28b854719e9aa07538aadd2829
70_10 <7010.dev@gmail.com>
2019-08-28 11:12:19 +0900

    [add] dotnet/http-client

---
+++ dotnet/http-client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace http_client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://example.com")
+            };
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "access-token");
+            client.DefaultRequestHeaders.Add("X-Client-ID", "client-1");
+
+            var req = new HttpRequestMessage(HttpMethod.Get, "/");
+            req.Headers.Add("X-Client-ID", "client-2");
+
+            var res = await client.SendAsync(req);
+
+            Console.WriteLine(res.RequestMessage.Headers);
+        }
+    }
+}
