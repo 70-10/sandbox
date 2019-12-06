7f514c2818520a313b7495d9f3be441b6de03707
70_10 <7010.dev@gmail.com>
2019-12-06 14:55:30 +0900

    [add] rust: variables

---
+++ rust/variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
+
+    let x = x * 2;
+    println!("The value of x is {}", x);
+
+    let x = "string";
+    println!("The value of x is {}", x);
+}
