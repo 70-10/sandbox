18d00e9423af8286fd7c70245f4e591dd7f1d320
70_10 <7010.dev@gmail.com>
2023-01-05 16:33:11 +0900

    Add css/grid-template-areas

---
+++ css/grid-template-areas/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section id="page">
+      <header>Header</header>
+      <nav>Navigation</nav>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </section>
+  );
+}
+
+export default App;
