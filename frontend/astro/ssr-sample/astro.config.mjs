f1c556c99a7450e3865e7224d4b0c0d43aa07963
70_10 <7010.dev@gmail.com>
2023-02-10 09:19:51 +0900

    Add astro/ssr-sample

---
+++ frontend/astro/ssr-sample/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
