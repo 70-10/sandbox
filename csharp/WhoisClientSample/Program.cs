39c5453d4947a355c607932deab93cdbeee9fcbd
70_10 <7010.dev@gmail.com>
2025-04-25 17:32:25 +0900

    Add csharp/WhoisClientSample

---
+++ csharp/WhoisClientSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+using Whois.NET;
+
+var options = new WhoisQueryOptions
+{
+    Encoding = Encoding.UTF8
+};
+var result = await WhoisClient.QueryAsync("google.com", options);
+
+Console.WriteLine(string.Join(" > ", result.RespondedServers));
+Console.WriteLine(result.Raw);
